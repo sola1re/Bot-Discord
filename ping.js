a8841bb1a9603eb55c7c84ae5b306edc1692c7b9
Paul Mandon <64035393+sola1re@users.noreply.github.com>
2020-10-04 10:40:48 +0200

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'ping',
+    description: "disa a ping command bitch nigga",
+    execute(message, args){
+        
+        
+        
+        message.channel.send('pong!');
+    }
+}
