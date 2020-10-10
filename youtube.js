a9e1263286bb87d257e3cc550178a2432c56fe7a
Paul Mandon <64035393+sola1re@users.noreply.github.com>
2020-10-10 19:15:25 +0200

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,17 +1,17 @@
 module.exports = {
     name: 'youtube',
-    description: "dis sends tsanium's youtube",
+    description: "this sends tsanium's youtube",
     execute(message, args){
 
         
         let role = message.guild.roles.cache.find(r => r.name === "Mod")
         
-        if (message.member.roles.cache.has('760542505334341712')){
+        if (message.member.roles.cache.some(r => r.name === "Mod")){
         message.channel.send('https://www.youtube.com/tsanium');
         }else{
             message.channel.send('You cant send this message because you dont have permission');
         }
     }
 }
-
+// you can also use message.member.roles.remove() to remove someone's role
 // check by the nam and not by the id: if (message.member.roles.cache.some(r => r.name === "Mod")
