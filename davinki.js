2a47fa1ed922f1259523168a1488643209aee639
Paul Mandon <64035393+sola1re@users.noreply.github.com>
2020-10-10 19:23:34 +0200

    Create davinki.js

---
+++ davinki.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: 'davinki',
+    description: "Monalisa? Davinki?",
+    execute(message, args){
+        
+        message.channel.send("Monalisa? Davinki? https://www.youtube.com/watch?v=BwA1c4XAb98");
+    
+        }
+        
+    }
+
